d90eb42f286ec9c214622c8e5eeda1fc122a615a
Краузе Кейсер <UnderOneBreath>
2024-02-14 03:08:02 +0900

    creat class client

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+public class Client {
+    public static void main(String[] args) {
+        // HERE Creat object organism
+        // Example Animal tigr = new Animal("Tigr", 5, "Lion");
+
+        //HERE object action Example moving or sound
+    }
+}
