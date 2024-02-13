ccbeac9132de262840016d1b260a63d924c1765f
Краузе Кейсер <UnderOneBreath>
2024-02-14 03:04:43 +0900

    creat and added structure Organism and Plant

--- src/Organism.java
+++ src/Organism.java
@@ -1,3 +1,14 @@
 public class Organism {
+    public String name;
+    public int age;
 
+    public Organism(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
 }
---
+++ src/Plant.java
@@ -0,0 +1,10 @@
+public class Plant extends Organism{
+    public String type;
+
+    public Plant(String name, int age, String type) {
+        super(name, age);
+        this.type = type;
+    }
+
+//    Need added action
+}
